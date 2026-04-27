7143fe5701bbb57669ff4463aa4faa35be58fd12
giribishnu200319-vu <giribishnu200319@gmail.com>
2026-04-27 15:43:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// using System;
+
+// //Main Program
+// class Program1
+// {
+//     static void Main() 
+//     {
+//         Console.WriteLine("Enter the number of terms in Fibonacci sequence: ");
+//         int n = Convert.ToInt32(Console.ReadLine());
+//         FibonacciResult fibonacci = new FibonacciResult(n);
+//         fibonacci.Calculate();
+//         Console.WriteLine (" Maximum value in the Fibonacci sequence: " + fibonacci.MaxValue);
+//         Console.WriteLine (" Total iterations: " + fibonacci.IterationCount);
+//     }
+// }
